21e3e6033ddefb1c38b735888d40ad3e18c744ba
gauris864 <47332251+gauris864@users.noreply.github.com>
2025-01-08 01:24:45 +0530

    Delete RewardService directory

--- RewardService/src/main/java/com/example/rewards/exception/DateFormatException.java
+++
@@ -1,9 +0,0 @@
-package com.example.rewards.exception;
-
-public class DateFormatException extends RuntimeException{
-	
-	public DateFormatException(String message) {
-        super(message);
-    }
-
-}
